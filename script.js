11e2ad7d7b4f96487ed9f2705c13305062d159d3
aditishukla297 <aditishukla29700@gmail.com>
2025-07-08 23:59:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,38 @@ const button = document.getElementById("search-button");
 const input = document.getElementById("city-input");
 
 const cityName = document.getElementById("city-name");
-const cityTime = document.getElementById("city-time");
-const cityTemp = document.getElementById("city-temp");
+const cityTime = document.getElementById("local-time");
+const cityTemp = document.getElementById("temperature");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherTitle = document.querySelector(".weather-title");
 
-async function getData(city) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=883329d53219483894a124530250807&q=${city}&aqi=yes`);
-    return await response.json();
-}
-
-button.addEventListener('click', async () => {
+const fetchWeatherData =  async () => {
     const value = input.value;
-    const result = await getData(value);
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=883329d53219483894a124530250807&q=${value}&aqi=yes`);
+    const result =  await response.json();
+    console.log(result)
     cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
     cityTime.innerText = result.location.localtime;
     cityTemp.innerText = `${result.current.temp_c}°C / ${result.current.temp_f}°F`;
+    weatherIcon.src = "https:" + result.current.condition.icon;
+    console.log(result.current.condition.text);
+    weatherTitle.innerHTML = capitalize(result.current.condition.text);
+}
+
+function capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+function capitalizeWords(text) {
+    return text
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+button.addEventListener('click',fetchWeatherData);
+
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        fetchWeatherData();
+    }
 });
